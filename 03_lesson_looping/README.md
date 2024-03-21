2f9937e1860cf7484404ec599a729f746717e096
loghen41 <logan@freemodel.com>
2024-03-20 23:27:48 -0600

    Looping logic

---
+++ 03_lesson_looping/README.md
@@ -0,0 +1,34 @@
+Looping
+----------------------
+
+## What is Looping?
+A language supports Looping when it provides syntax to complete a section of code repeatedly
+
+As in, A section of code will be repeatedly run based on the looping syntax
+
+For example: 
+  - We have a list of numbers
+  - We want to add the list of numbers together into a single number
+  - We go through the list of numbers and add them together into a single value
+
+## Common Conditionals
+The most common loops include:
+- for 
+- for ... in
+- while
+
+
+## Lesson structure
+Each lesson will show the process of
+- Information about the most common conditionals
+  - for 
+  - for ... in
+
+
+## Conditionals in Depth
+Conditionals are fairly simple as a concept. If you desire additional information, refer to [Wikipedia](https://en.wikipedia.org/wiki/For_loop).
+
+## In summary
+Looping syntax was established in order to:
+- Support repeated logic in blocks of code
+- There are multiple types of looping logic, the syntax and types supported will vary from language to language
---
+++ 03_lesson_looping/main.js
@@ -0,0 +1,67 @@
+// Exercise 1. As a recap to last week, please instantiate and initialize a variable. (doesn't matter the type)
+// Then use that variable in an if, else if, else statement
+
+console.log("1. Exercise one answer below -------------------");
+
+// For loop
+// Loops build the ability to create a repeating action in code
+// For example, the for loop statement below prints out a statement each 10 times
+
+for (let i = 0; i < 10; i++) {
+	console.log(`my number: ${i}`);
+}
+
+// To understand this in detail, we are going to break down the loop into separate stages.
+
+// Exercise 2. Declare two integers, it doesn't matter what the values are, then print them out
+// For example
+
+let myInt = 503;
+let mySecondInt = 832;
+console.log(`My intergers are: ${myInt} ${mySecondInt}`);
+
+console.log("2. Exercise two values below ----------------");
+
+// How does the for loop work?
+// Our programming language uses a special operator to designate when a statement ends the semicolon ;
+// A for loop is actually just three programming statements all put together on one line
+// statement 1: A variable is declared (usually an integer)
+// Statement 2: A stopping point is delcared (usually a comparison with another integer)
+// Statement 3: An action is delcared that adjusts the variable in step one to make it reach the stopping condition
+
+// Broken down, the for loop looks like this:
+
+for (let myVariable = 503;     // The initial variable is declared
+	myVariable < 832;      // A stopping condition is set
+	myVariable++) {        // An action is done to make the variable reach the stopping condition
+	// The block of code
+	console.log(`myNumber ${myVariable}`);
+}
+
+// Using our variables from above, it looks like this:
+for (let i = myInt; i < mySecondInt; i++) {
+	console.log(`my Int: ${i}`);
+}
+
+// Exercise 3. Write a for loop similar to what was defined above, but using your variables instead
+console.log("3. Exercise three statements below ----------------");
+
+// For In loop
+// A for ... in loop allows you to loop over the contents of a larger structure. Such as an array.
+// The Javascript version of for ... in is by using a for ... of
+// For example:
+
+let myFruit = ["Apple", "Banana", "Pear", "Mango"];
+
+for (let fruit of myFruit) {
+	console.log(`my fruit: ${fruit}`);
+}
+
+// Exercise 4. Write an array and then loop through the array using the `for range` syntax
+
+console.log("4. Exercise four statements below ----------------");
+
+// In summary
+// Looping allow for logic to be executed repatedly, common loops are:
+// - for
+// - for ... in
